7e7e7f37bb92c9b0f056f8199bb29bd0d1b3a82d
Alex <trofimuk.alex@gmail.com>
2024-04-24 14:54:30 +0300

    added new mime type - svg

--- wp-content/themes/themo/functions.php
+++ wp-content/themes/themo/functions.php
@@ -3195,3 +3195,9 @@ if ( ! function_exists( 'foton_mikado_framework' ) ) {
 		return FotonMikadoClassFramework::get_instance();
 	}
 }
+
+function my_myme_types($mime_types){
+    $mime_types['svg'] = 'image/svg+xml'; //Adding svg extension
+    return $mime_types;
+}
+add_filter('upload_mimes', 'my_myme_types', 1, 1);

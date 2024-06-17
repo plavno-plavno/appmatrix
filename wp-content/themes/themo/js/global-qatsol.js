d3d144c2e2db5725227ee417a7f5d73fc52720be
dmitry.bobrov <dmitry@motionarray.com>
2024-06-17 12:59:53 +0300

    fix styles responsive

--- wp-content/themes/themo/js/global-qatsol.js
+++ wp-content/themes/themo/js/global-qatsol.js
@@ -56,21 +56,6 @@ document.addEventListener("DOMContentLoaded",() => {
         scrollPrev = scrolled;
     });
 
-
-    let swiper = new Swiper(".techSwiper", {
-        slidesPerView: 1.2,
-        spaceBetween: 2,
-        breakpoints: {
-            768: {
-                slidesPerView: 2.4,
-            },
-
-            1100: {
-                slidesPerView: 4,
-            },
-        },
-    });
-
     openAccordion(accordionMenu);
 
 

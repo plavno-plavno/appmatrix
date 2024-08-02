f6e1feb586b2903bf990997336fb37c8870bd90c
Tatsiana Silchenko <dmitry@motionarray.com>
2024-08-02 11:38:55 +0300

    edit template for solutions

--- wp-content/themes/themo/single-solutions.php
+++ wp-content/themes/themo/single-solutions.php
@@ -9,9 +9,6 @@
             <div class="entry-content <?php echo ideothemo_get_theme_skin_class(); ?> <?php echo ideothemo_get_blog_sidebar_page_classes(ideothemo_get_sidebar_position()); ?>">
                 <?php the_content(); ?>
             </div>
-            <?php
-            get_sidebar();
-            ?>
         </div>
 
     <?php if (!ideothemo_is_boxed_version() && (ideothemo_is_sidebar_enabled() || !has_shortcode(get_the_content(), 'vc_row'))): ?>

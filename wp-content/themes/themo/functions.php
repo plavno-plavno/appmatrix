d54d6ebe4ba04c9b7de9d23d19ea95450ce53829
Tatsiana Silchenko <dmitry@motionarray.com>
2024-10-04 17:27:43 +0300

    blog carousel

--- wp-content/themes/themo/functions.php
+++ wp-content/themes/themo/functions.php
@@ -3628,8 +3628,14 @@ function latest_posts_carousel_shortcode()
                     <div class="vc_col-sm-6 h2-title">
                         <h2 class="title"><span>Our latest insights</span></h2>
                     </div>
-                    <div class="vc_col-sm-6 title-link">
-                        <a href="/blog">Explore our blog to learn more about tech & people</a>
+                    <div class="vc_col-sm-6 title-link common-link">
+                        <a href="/blog">
+                            <span>Explore our blog to learn more about tech & people</span>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5 12H19" stroke="#EAC571" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                <path d="M12 5L19 12L12 19" stroke="#EAC571" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                        </a>
                     </div>
                 </div>
                 <div class="swiper-wrapper">
